1ea94cdb25b4b397101809da5e3c886a31f5e5be
Henry Larson <hxl1116@g.rit.edu>
2019-10-07 18:52:47 -0400

    Updated pom to resolve dependency issues.

--- src/evo/io/DiskManager_ESTest.java
+++ src/evo/io/DiskManager_ESTest.java
@@ -19,7 +19,7 @@ import org.evosuite.runtime.testdata.FileSystemHandling;
 import org.junit.runner.RunWith;
 import view.PersonTableModel;
 
-@RunWith(EvoRunner.class) @EvoRunnerParameters(mockJVMNonDeterminism = true, useVFS = true, useVNET = true, resetStaticState = true, separateClassLoader = true, useJEE = true) 
+@RunWith(EvoRunner.class) @EvoRunnerParameters(mockJVMNonDeterminism = true, useVFS = true, useVNET = true, resetStaticState = true, separateClassLoader = false, useJEE = true)
 public class DiskManager_ESTest extends io.DiskManager_ESTest_scaffolding {
 
   @Test(timeout = 4000)
--- src/evo/model/Person_ESTest.java
+++ src/evo/model/Person_ESTest.java
@@ -22,8 +22,8 @@ import org.evosuite.runtime.mock.java.util.MockCalendar;
 import org.evosuite.runtime.mock.java.util.MockGregorianCalendar;
 import org.junit.runner.RunWith;
 
-@RunWith(EvoRunner.class) @EvoRunnerParameters(mockJVMNonDeterminism = true, useVFS = true, useVNET = true, resetStaticState = true, separateClassLoader = true, useJEE = true) 
-public class Person_ESTest extends Person_ESTest_scaffolding {
+@RunWith(EvoRunner.class) @EvoRunnerParameters(mockJVMNonDeterminism = true, useVFS = true, useVNET = true, resetStaticState = true, separateClassLoader = false, useJEE = true)
+public class Person_ESTest extends model.Person_ESTest_scaffolding {
 
   @Test(timeout = 4000)
   public void test00()  throws Throwable  {
